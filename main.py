445e323f0ae4cc86919d0b0fb41fffa74863cd73
henning.bunk <henning.bunk@wetteronline.de>
2020-12-02 09:57:37 +0100

    read in line with lines already split user xor

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import re
 
+
 def read_input(filename):
-    with open(filename, 'r') as file:
-        return file.read()
+    return [line for line in open(filename, 'r')]
 
 
 if __name__ == '__main__':
     input = read_input('2.txt')
 
     count = 0
-    for line in input.split('\n'):
+    for line in input:
         match = re.match(r'(\d+)-(\d+) (\w): (\w+)', line)
         min = int(match[1])
         max = int(match[2])
         letter = match[3]
         password = match[4]
 
-        if password[min - 1] == letter and password[max - 1] != letter:
-            count += 1
-        elif password[max - 1] == letter and password[min - 1] != letter:
+        if (password[min - 1] == letter) ^ (password[max - 1] == letter):
             count += 1
 
     print(count)
