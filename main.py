dad7d9e655d14bbe7b43201993151fc16ca53fdb
henning.bunk <henning.bunk@wetteronline.de>
2020-12-03 17:43:41 +0100

    3-1

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import re
 
 
-def read_input(filename):
+def read_input_as_list(filename):
     return [line for line in open(filename, 'r')]
+
+def read_input(filename):
+    return open(filename, 'r').read()
+
+
+def count_trees(input):
+    my_position = 0
+    trees = 0
+    for line in input.split('\n'):
+        if line[my_position] == '#':
+            trees += 1
+        my_position = (my_position + 3) % len(line)
+    return trees
 
 
 if __name__ == '__main__':
-    input = read_input('2.txt')
-
-    count = 0
-    for line in input:
-        match = re.match(r'(\d+)-(\d+) (\w): (\w+)', line)
-        min = int(match[1])
-        max = int(match[2])
-        letter = match[3]
-        password = match[4]
-
-        if (password[min - 1] == letter) ^ (password[max - 1] == letter):
-            count += 1
-
-    print(count)
+    input = read_input('3.txt')
+    print(count_trees(input))
---
+++ test_test.py
@@ -0,0 +1,17 @@
+from main import count_trees
+
+sample_input = """..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"""
+
+
+def test_answer():
+    assert count_trees(sample_input) == 7
